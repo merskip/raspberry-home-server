e8c1f006369b1dd6415cd95ad1b811b9679a6b5a
Piotr Merski <merskip@gmail.com>
2019-08-18 09:42:03 +0200

    Fetching sensors

--- src/main/webapp/app.jsx
+++ src/main/webapp/app.jsx
@@ -1,17 +1,36 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
 
-
 class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {sensors: []};
+    }
+
+    componentDidMount() {
+        fetch('/api/sensors')
+            .then(res => res.json())
+            .then((data) => {
+                this.setState({sensors: data})
+            });
+    }
 
     render() {
         return (
-            <h1>It works! :-)</h1>
+            <div>
+                <h2>Sensors</h2>
+                <ul>
+                    {this.state.sensors.map((sensor) => (
+                        <li>{sensor.name}</li>
+                    ))}
+                </ul>
+            </div>
         )
     }
 }
 
 ReactDOM.render(
-    <App />,
+    <App/>,
     document.getElementById('app')
 );
