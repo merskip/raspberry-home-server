612512fd6ff65a6efaadf8475365bee556c13fa7
Piotr Merski <merskip@gmail.com>
2019-09-14 19:36:09 +0200

    Characteristics values

--- src/main/webapp/src/page/HomePage.js
+++ src/main/webapp/src/page/HomePage.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 import ActivityIndicatorView from "../view/ActivityIndicatorView";
 
 export default class HomePage extends Component {
@@ -6,33 +6,62 @@ export default class HomePage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            sensors: null
+            sensors: null,
+            lastMeasurements: null
         };
     }
 
     componentDidMount() {
         fetch(`api/sensors`)
             .then(res => res.json())
-            .then(result => {
+            .then(sensors => {
+                console.log(sensors);
                 this.setState({
-                    sensors: result
-                })
+                    sensors: sensors
+                });
+
+                fetch(`api/sensors/${sensors.map(s => s['id']).join(',')}/lastMeasurements`)
+                    .then(res => res.json())
+                    .then(measurements => {
+                        this.setState({
+                            lastMeasurements: measurements
+                        });
+                    });
             });
     }
 
     render() {
-        return (<div className="container">
+        return <div className="container">
             {!this.state.sensors ? <ActivityIndicatorView/>
-                : <SensorsGrid sensors={this.state.sensors}/>}
-        </div>)
+                : <SensorsGrid sensors={this.state.sensors} lastMeasurements={this.state.lastMeasurements}/>}
+        </div>
     }
 }
 
 function SensorsGrid(props) {
-    return (<div className="row">{
-        props.sensors.map((item, index) => (
-            <div className="col-4" key={index}>{index}: {item['name']}</div>
-        ))
-    }
-    </div>)
+    return <div className="card-columns">
+        {props['sensors'].map((sensor, index) => (
+            <div key={index} className="card">
+                <div className="card-body">
+                    <h5 className="card-title">{sensor['name']}</h5>
+                    {sensor['characteristics'].map((characteristic, index) => {
+                        let lastMeasurement = (props['lastMeasurements'] || []).find(m => m['characteristicId'] === characteristic['id']);
+                        return (
+                            <CharacteristicValue key={index} sensor={sensor} characteristic={characteristic}
+                                                 lastMeasurement={lastMeasurement}/>
+                        )
+                    })}
+                </div>
+            </div>
+        ))}
+    </div>
+}
+
+function CharacteristicValue(props) {
+    return <div className="mt-2">{props['characteristic']['name']}<br/>
+            {!props['lastMeasurement']
+                ? <ActivityIndicatorView spinner-grow/>
+                : <strong>{props['lastMeasurement']['formattedValue']} {props['characteristic']['unit']}</strong>
+            }
+        </div>
 }
--- src/main/webapp/src/view/ActivityIndicatorView.js
+++ src/main/webapp/src/view/ActivityIndicatorView.js
@@ -1,9 +1,14 @@
-import React, {Component} from "react";
+import React from "react";
 
-export default function ActivityIndicatorView() {
-    return <div className="text-center">
-        <div className="spinner-border text-primary" role="status">
-            <span className="sr-only">Loading...</span>
-        </div>
-    </div>
+export default function ActivityIndicatorView(props) {
+    return (<div className="text-center">
+        {props['small']
+            ? <div className="spinner-grow text-primary spinner-grow-sm" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+            : <div className="spinner-border text-primary" role="status">
+                <span className="sr-only">Loading...</span>
+            </div>
+        }
+    </div>)
 }
